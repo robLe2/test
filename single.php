bdae119983d4107c8469be028af628369ac5f85c
robLe2 <robin.meunierjacquin@gmail.com>
2020-10-09 15:17:24 +0200

    ajout du single.php

---
+++ single.php
@@ -0,0 +1,22 @@
+<?php get_header();?>
+<section id="container">
+<aside>
+    <?php get_sidebar();?>
+</aside>
+
+<!-- The Loop -->
+<?php if(have_posts()):
+    while(have_posts()):
+        the_post()?>
+
+<article>
+        <h2><?php the_title();?></h2>
+        <div><?php the_content();?></div>
+        <p><?php the_time();?></p>
+        <p><?php the_author_posts_link();?></p>
+</article>
+    <?php endwhile; else:?>
+    <p>Pas d'articles</p>
+    <?php endif?>
+</section>
+<?php get_footer();?>
